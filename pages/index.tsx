b8d75d8547649c7a549830b555ef5e987d3877c5
Philani Ncube <ncbphi001@gmail.com>
2022-08-01 13:57:03 +0200

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,83 +4,35 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div
+      className="flex min-h-screen flex-col items-center justify-center px-3 py-2"
+      style={{ backgroundColor: "hsl(30, 38%, 92%)" }}
+    >
+      <div className="max-w-[300px] md:max-w-xl mx-auto rounded-xl overflow-hidden shadow-xl shadow-orange-200/30 grid grid-cols-1 md:grid-cols-2">
+        <img
+          className="w-full object-cover h-[300px] md:h-full"
+          src="/images/image-product-desktop.jpg"
+        />
+        <div className="w-full bg-white p-6">
+          <p className="uppercase text-gray-500 text-md">Perfume</p>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+          <h2 className="text-gray-800 text-4xl font-extrabold">
+            Gabrielle Essence Eau De Parfum
+          </h2>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+          <p className="text-gray-400 mt-6 mb-4 text-sm">
+            A floral, solar and voluptuous interpretation composed by Olivier
+            Polge, Perfumer-Creator for the House of CHANEL
+          </p>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
+          <span className="flex space-x-5 items-center"><p className="text-green-800 text-3xl font-extrabold">$149.99</p><p className="text-sm text-gray-500 line-through">$169.99</p></span>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+          <button className="w-full flex space-x-3 justify-center items-center py-3 mt-6 rounded-lg bg-green-700 hover:bg-green-900 text-white"><img src="../images/icon-cart.svg" className="h-6 w-6" /><p className="text-white font-medium">Add To Cart</p></button>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home

0ad8dded886f2de5cb2b7eec404ee74f59d9bd9e
Philani Ncube <ncbphi001@gmail.com>
2022-08-01 14:10:33 +0200

    fixed issues

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,10 +8,14 @@ const Home: NextPage = () => {
       className="flex min-h-screen flex-col items-center justify-center px-3 py-2"
       style={{ backgroundColor: "hsl(30, 38%, 92%)" }}
     >
+      <Head>
+        <title>Product Card</title>
+      </Head>
       <div className="max-w-[300px] md:max-w-xl mx-auto rounded-xl overflow-hidden shadow-xl shadow-orange-200/30 grid grid-cols-1 md:grid-cols-2">
         <img
           className="w-full object-cover h-[300px] md:h-full"
           src="/images/image-product-desktop.jpg"
+          alt="Product Card"
         />
         <div className="w-full bg-white p-6">
           <p className="uppercase text-gray-500 text-md">Perfume</p>
@@ -25,9 +29,9 @@ const Home: NextPage = () => {
             Polge, Perfumer-Creator for the House of CHANEL
           </p>
 
-          <span className="flex space-x-5 items-center"><p className="text-green-800 text-3xl font-extrabold">$149.99</p><p className="text-sm text-gray-500 line-through">$169.99</p></span>
+          <div className="flex space-x-5 items-center"><p className="text-green-800 text-3xl font-extrabold">$149.99</p><p className="text-sm text-gray-500 line-through">$169.99</p></div>
 
-          <button className="w-full flex space-x-3 justify-center items-center py-3 mt-6 rounded-lg bg-green-700 hover:bg-green-900 text-white"><img src="../images/icon-cart.svg" className="h-6 w-6" /><p className="text-white font-medium">Add To Cart</p></button>
+          <button className="w-full flex space-x-3 justify-center items-center py-3 mt-6 rounded-lg bg-green-700 hover:bg-green-900 text-white"><img src="../images/icon-cart.svg" className="h-6 w-6" alt="icon" /><span className="text-white font-medium">Add To Cart</span></button>
 
         </div>
       </div>
